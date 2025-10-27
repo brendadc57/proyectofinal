d37a2f01662eea59e0447eaeaf8af634e5feae82
brendadc57 <b.delcastillo@ebc.edu.mx>
2025-10-27 12:44:43 -0600

    Nueva versión 27 octubre

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# (Opcional si usas la carpeta en la raíz)
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- prospects/admin.py
+++ prospects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Prospect
+
+@admin.register(Prospect)
+class ProspectoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo', 'telefono', 'estatus')
+    search_fields = ('nombre', 'correo')
+    list_filter = ('estatus',)
